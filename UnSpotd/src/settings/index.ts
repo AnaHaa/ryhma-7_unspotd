38006d1266c73d36ff29e9ed6bee873a7b112928
AnttiH <anttip.haarala@gmail.com>
2021-11-11 10:18:42 +0200

    R7V-22 Add http connection

---
+++ UnSpotd/src/settings/index.ts
@@ -0,0 +1,20 @@
+import connection from '../../connection.json';
+
+export default class Settings {
+    userEndPoint: string | undefined;
+    visitEndPoint: string | undefined;
+    apiKey: string | undefined;
+
+    /**
+     * Fetch configuration information
+     */
+    constructor() {
+        try {
+            this.userEndPoint = connection.userUrl;
+            this.visitEndPoint = connection.visitsUrl;
+            this.apiKey = connection.apikey;
+        } catch (e) {
+            throw new Error('Missing connection information');
+        }
+    }
+}
